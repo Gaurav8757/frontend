66d4dbb78cd4ce76536ca2a193427b967bb5e6d2
Gaurav Kumar <mr.gauravkumar8757@gmail.com>
2024-08-10 19:13:24 +0530

    added disable copy paste contextmenu, added login button in mobile view

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,10 +1,10 @@
 /* eslint-disable react/prop-types */
-import Sidebar from './Sidebar';
+import Sidebar from "./Sidebar";
 import { useState, startTransition } from "react";
-import { NavLink } from 'react-router-dom';
+import { NavLink, Link } from "react-router-dom";
 function classNames(...classes) {
   // console.log(classes.filter(Boolean).join(' '));
-  return classes.filter(Boolean).join(' ');
+  return classes.filter(Boolean).join(" ");
 }
 
 export default function Navbar({ navigation }) {
@@ -12,37 +12,41 @@ export default function Navbar({ navigation }) {
 
   const toggleSidebar = () => {
     startTransition(() => {
-    setSidebarOpen(!isSidebarOpen);
-  });
+      setSidebarOpen(!isSidebarOpen);
+    });
   };
 
   return (
     <>
       {/* Navbar */}
-      <nav style={{backgroundColor: "#A31217"}}>
+      <nav style={{ backgroundColor: "#A31217" }}>
         <div className="mx-auto max-w-auto px-2">
-          <div className="relative flex h-20 items-center justify-between">
-            <div className="absolute inset-y-0 left-0 flex items-center sm:justify-center  sm:hidden">
+          <div className="relative flex  h-20 items-center justify-between">
+            <div className=" inset-y-0 left-0 flex items-center sm:justify-center jus  sm:hidden">
               {/* Mobile menu button*/}
               <button
                 className="relative inline-flex items-center justify-center rounded-lg  text-white hover:bg-red-500 "
                 onClick={toggleSidebar}
               >
-               
                 <span className="sr-only">Open main menu</span>
                 {isSidebarOpen ? (
                   // <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
-                  <img src="/off.png" height={5} width={25} alt="staff"/>
+                  <img src="/off.png" height={5} width={25} alt="staff" />
                 ) : (
-                  <img src="/app.png" height={5} width={25} alt="leger"/>
+                  <img src="/app.png" height={5} width={25} alt="leger" />
                   // <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
                 )}
               </button>
             </div>
-            <div className="flex flex-1  items-center sm:justify-center justify-evenly sm:items-stretch md:justify-between">
-              <NavLink to="/" className="flex ms-4 md:me-2  items-center">
-                <img src="/logo.webp" rel='preload' loading='lazy' className=" h-16  py-2 xs:h-12 sm:h-12 md:h-16   lg:h-16   xl:h-20  mx-2 w-28 sm:w-28 md:w-28 lg:w-32 xl:w-32 " alt="Logo" />
-               
+           
+              <NavLink to="/" className="flex ms-4 md:me-2 justify-center items-center">
+                <img
+                  src="/logo.webp"
+                  rel="preload"
+                  loading="lazy"
+                  className=" h-16 items-center py-2 xs:h-12 sm:h-12 md:h-16   lg:h-16   xl:h-20  mx-2 w-28 sm:w-28 md:w-28 lg:w-32 xl:w-32 "
+                  alt="Logo"
+                />
               </NavLink>
 
               <div className="hidden sm:ml-2  items-center sm:block md:block py-10">
@@ -54,12 +58,12 @@ export default function Navbar({ navigation }) {
                         className={({ isActive }) =>
                           classNames(
                             isActive
-                              ? ' text-slate-200   sm:text-sm md:text-sm lg:text-sm xl:text-lg'
-                              : 'bg-gradient-to-r from-slate-100 via-slate-100 to-slate-200 bg-clip-text text-transparent hover:bg-orange-600 ',
-                            'rounded-md px-2 py-2 font-medium text-gray-900'
+                              ? " text-slate-200   sm:text-sm md:text-sm lg:text-sm xl:text-lg"
+                              : "bg-gradient-to-r from-slate-100 via-slate-100 to-slate-200 bg-clip-text text-transparent hover:bg-orange-600 ",
+                            "rounded-md px-2 py-2 font-medium text-gray-900"
                           )
                         }
-                        aria-current={item.current ? 'page' : undefined}
+                        aria-current={item.current ? "page" : undefined}
                       >
                         {item.name}
                       </NavLink>
@@ -101,18 +105,18 @@ export default function Navbar({ navigation }) {
                   ))}
                 </div>
               </div>
-
-
-              <div className="absolute inset-y-0 right-0 z-40 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-                {/* Profile dropdown */}
+                     <div className="flex z-40 justify-end items-end">
+                <Link to = "/login" className="relative xl:hidden sm:hidden inline-flex items-center justify-end p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-pink-500 to-orange-400 group-hover:from-pink-500 group-hover:to-orange-400 hover:text-white dark:text-white focus:ring-0 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800">
+                  <span className="relative px-2 py-1 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+                    Login
+                  </span>
+                </Link>
               </div>
             </div>
           </div>
-        </div>
+       
         <Sidebar navigation={navigation} isSidebarOpen={isSidebarOpen} />
       </nav>
-
-
     </>
   );
 }
